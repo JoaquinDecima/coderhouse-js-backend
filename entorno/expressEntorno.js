5a3a73a876a364c0a435e810503320840821d7b0
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-09-24 08:38:04 -0300

    Add Entorno script

---
+++ entorno/expressEntorno.js
@@ -0,0 +1,40 @@
+import Contenedor from '../src/filemanager/contenedor.js'
+
+// Funciones auxiliares
+
+function getProduct(nombre, precio, imagen) {
+  return({
+    title : nombre,
+    price : precio,
+    thumbnail: imagen
+  })
+}
+const print = console.log
+
+// Se prepara entorno
+
+const producto = getProduct('Escuadra', 123.45, 'https://patojad.com.ar/nof.svg');
+const producto2 = getProduct('Calculadora', 234.56, 'https://serscout.com.ar/nof.svg');
+const producto3 = getProduct('Globo Terraqueo', 345.67, 'https://lynx.net.ar/nof.svg');
+
+const micont = new Contenedor('./productos.txt');
+
+print('##############################################################');
+print('##                                                          ##');
+print('##                      INICIO DE TEST                      ##');
+print('##                                                          ##');
+print('##############################################################');
+print('');
+print('');
+print('AGREGO UN ELEMENTO Y REVISO EL FILE');
+print('--------------------------------------------------------------');
+print('');
+
+let id = micont.save(producto);
+print(`Se agrego elemento id: ${id}`);
+id = micont.save(producto2);
+print(`Se agrego elemento id: ${id}`);
+id = micont.save(producto3);
+print(`Se agrego elemento id: ${id}`);
+let printeable = micont.getAll();
+print(printeable);
