dd9401cb2c5a464deefd0c7324dcd76722a704eb
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-10-20 10:11:18 -0300

    Se extrae router producto

--- src/index.js
+++ src/index.js
@@ -1,54 +1,17 @@
 import express from 'express';
-import Contenedor from './filemanager/contenedor.js';
-import startEntorno from '../entorno/expressEntorno.js';
+import routerProductos from './routers/routerProductos.js'
 
 // SetUp del entorno
 const app = express();
 const port = process.env.PORT || 8080
-const cont = new Contenedor('../productos.txt');
 const router = express.Router();
 
-startEntorno(cont);
 app.use(express.static('./public'));
 app.use(express.json());
-app.use('/api',router);
 
 // Se configura API
 
-router.get('/productos', function(req, res, next){
-  res.send(cont.getAll());
-});
-
-router.get('/productoRandom', function(req, res, next){
-  const random = Math.floor(Math.random() * cont.getAll().length);
-  res.send(cont.getByID(random));
-});
-
-router.get('/productos/:id', function(req, res, next){
-  res.send(cont.getByID(req.params.id));
-});
-
-router.post('/productos', function(req, res, next){
-  console.log(req.body)
-  const id = cont.save({
-    title : req.body.nombre,
-    price : req.body.precio,
-    thumbnail: req.body.imagen
-  })
-  res.send({id: id});
-});
-
-router.delete('/productos/:id', function(req, res, next){
-  res.send(cont.deleteById(req.params.id));
-});
-
-router.put('/productos', function(req, res, next){
-  if(cont.editByID(req.body.id, req.body.nombre, req.body.precio, req.body.imagen)){
-    res.send("");
-  }else{
-    res.send({error:'Producto no encontrado'});
-  }
-});
+app.use('/api/productos',routerProductos);
 
 // Se inicia API
 
---
+++ src/routers/routerProductos.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import Contenedor from '../filemanager/contenedor.js';
+import startEntorno from '../../entorno/expressEntorno.js';
+
+const routerProductos = express.Router();
+const cont = new Contenedor('../../productos.txt');
+startEntorno(cont);
+
+routerProductos.get('/', function(req, res, next){
+  res.send(cont.getAll());
+});
+
+routerProductos.get('/:id', function(req, res, next){
+  res.send(cont.getByID(req.params.id));
+});
+
+routerProductos.post('/', function(req, res, next){
+  console.log(req.body)
+  const id = cont.save({
+    title : req.body.nombre,
+    price : req.body.precio,
+    thumbnail: req.body.imagen
+  })
+  res.send({id: id});
+});
+
+routerProductos.delete('/:id', function(req, res, next){
+  res.send(cont.deleteById(req.params.id));
+});
+
+routerProductos.put('/', function(req, res, next){
+  if(cont.editByID(req.body.id, req.body.nombre, req.body.precio, req.body.imagen)){
+    res.send("");
+  }else{
+    res.send({error:'Producto no encontrado'});
+  }
+});
+
+routerProductos.get('/random', function(req, res, next){
+  const random = Math.floor(Math.random() * cont.getAll().length);
+  res.send(cont.getByID(random));
+});
+
+export default routerProductos
