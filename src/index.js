ba56231da7d529e5016675c192ac6fea7afeda00
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-10-22 11:02:09 -0300

    Move to EJS

--- src/index.js
+++ src/index.js
@@ -6,12 +6,17 @@ const app = express();
 const port = process.env.PORT || 8080
 const router = express.Router();
 
-app.use(express.static('./public'));
+app.set('view engine', 'ejs')
+// app.use(express.static('./public'));
 app.use(express.json());
 
 // Se configura API
 
 app.use('/api/productos',routerProductos);
+
+app.get('/', (req,res)=>{
+  res.render('index', req)
+})
 
 // Se inicia API
 
