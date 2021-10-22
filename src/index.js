da120696710ad3535ab1f562d85cd7514a2a79ec
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-10-22 18:19:45 -0300

    Remove API REST

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import express from 'express';
-import routerProductos from './routers/routerProductos.js'
+import routerProductos from './routers/routerProductos.js';
 
 // SetUp del entorno
 const app = express();
-const port = process.env.PORT || 8080
-const router = express.Router();
+const port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs')
 // app.use(express.static('./public'));
-app.use(express.json());
+// app.use(express.json());
 
 // Se configura API
 
-app.use('/api/productos',routerProductos);
+// app.use('/api/productos',routerProductos);
 
 app.get('/', (req,res)=>{
-  res.render('index', {req})
+  res.render('index')
 })
 
 // Se inicia API
