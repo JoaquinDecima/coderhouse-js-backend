fb202b609a0b2af55c01221a4879295b0cc1423b
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-10-26 16:06:32 -0300

    Add Socket IO Add HTTP

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import exphbs from 'express-handlebars';
+import { Server as HTTPServer } from 'http';
+import { Server as IOServer } from 'socket.io';
 // import routerProductos from './routers/routerProductos.js';
 import Contenedor from './filemanager/contenedor.js';
 import startEntorno from '../entorno/expressEntorno.js';
@@ -9,7 +11,9 @@ startEntorno(cont);
 
 // SetUp del entorno
 const app = express();
-const port = process.env.PORT || 8080;
+const httpServer = new HTTPServer(app);
+const io = new IOServer(httpServer);
+const PORT = process.env.PORT || 8080;
 
 app.engine('hbs', exphbs({
   extname: 'hbs',
@@ -42,5 +46,8 @@ app.post('/productos/', (req,res)=>{
 
 // Se inicia API
 
-app.listen(port);
-console.log(`Inicio pode verlo en http://localhost:${port}`);
+const conectedServer = httpServer.listen(PORT, () => {
+  console.log(`Inicio pode verlo en http://localhost:${PORT}`);
+})
+
+conectedServer.on('error', error => console.log(`error en servidor ${error}`))
